ac34a02588bb03e78566cd84a5bdd05ef0947e69
Addie Brunk <mlbrunk23@gmail.com>
2018-05-29 00:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ function johnLennonFacts(facts){
   let anArr = [];
   let i = 0;
   while (facts[i]){
-
     anArr.push(`${facts[i]}!!!`)
     i++
+  }
+  return anArr;
+}
+
+function iLoveTheBeatles(number){
+  let anArr = [];
+  if (number < 15){
+    let number = 0;
+    do {
+      (anArr.push('I love the Beatles!'))
+      number++;
+    }while (number > 0);
   }
   return anArr;
 }
